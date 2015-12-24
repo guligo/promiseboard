834f6a74a6b00427354af54e21d13316eb2a9f96
guligo <igors.gulbinskis@gmail.com>
2015-12-24 15:44:18 +0100

    Updates

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(8081, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("HTTP server listening at http://%s:%s", host, port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
