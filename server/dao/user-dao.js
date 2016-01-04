ecf8748b0104c57db01af47f6eb6110936c97b02
guligo <igors.gulbinskis@gmail.com>
2016-01-04 21:38:28 +0200

    Issue #14

--- server/dao/user-dao.js
+++ server/dao/user-dao.js
@@ -3,7 +3,7 @@ const DATABASE_URL = 'postgres://postgres:secret@localhost:5432/promiseboard';
 var pg = require('pg');
 
 var _init = function(callback) {
-    console.log('Initializing user DAO');
+    console.log('Initializing persistence layer for user');
 
     pg.connect(process.env.DATABASE_URL || DATABASE_URL, function(err, client) {
         if (err) throw err;
