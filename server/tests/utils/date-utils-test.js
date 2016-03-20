63221dcf4c51c7e110a3261d3a5a57c2f3bad000
guligo <igors.gulbinskis@gmail.com>
2016-03-20 09:36:40 +0100

    Issue #68

--- server/tests/utils/date-utils-test.js
+++ server/tests/utils/date-utils-test.js
@@ -37,4 +37,87 @@ describe('Test date-utils.js', function() {
 
     });
 
+    describe('#calculateTimeLeft(dueDate)', function() {
+
+        it('should return "2 days, 2 hours, 30 minutes"', function() {
+            var date = new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000 + 2 * 60 * 60 * 1000 + 30 * 60 * 1000);
+
+            var assertion = (
+                '2 days, 2 hours, 29 minutes' === dateUtils.calculateTimeLeft(date) ||
+                '2 days, 2 hours, 30 minutes' === dateUtils.calculateTimeLeft(date)
+            );
+            assert.equal(true, assertion);
+        });
+
+    });
+
+    describe('#dateBefore(date1, date2)', function() {
+
+        it('should return "true" if date1 is 1 second before date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateBefore(date, new Date(date.getTime() + 1000)));
+        });
+
+        it('should return "false" if date1 equals date2', function() {
+            var date = new Date();
+            assert.equal(false, dateUtils.dateBefore(date, date));
+        });
+
+    });
+
+    describe('#dateBeforeOrEquals(date1, date2)', function() {
+
+        it('should return "true" if date1 is 3 second before date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateBeforeOrEquals(date, new Date(date.getTime() + 3 * 1000)));
+        });
+
+        it('should return "true" if date1 equals date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateBeforeOrEquals(date, date));
+        });
+
+    });
+
+    describe('#dateAfter(date1, date2)', function() {
+
+        it('should return "true" if date1 is 1 second after date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateAfter(date, new Date(date.getTime() - 1000)));
+        });
+
+        it('should return "false" if date1 equals date2', function() {
+            var date = new Date();
+            assert.equal(false, dateUtils.dateAfter(date, date));
+        });
+
+    });
+
+    describe('#dateAfterOrEquals(date1, date2)', function() {
+
+        it('should return "true" if date1 is 3 second after date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateAfterOrEquals(date, new Date(date.getTime() - 3 * 1000)));
+        });
+
+        it('should return "true" if date1 equals date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateBeforeOrEquals(date, date));
+        });
+
+    });
+
+    describe('#dateEquals(date1, date2)', function() {
+
+        it('should return "true" if date1 equals date2', function() {
+            var date = new Date();
+            assert.equal(true, dateUtils.dateEquals(date, date));
+        });
+
+        it('should return "false" if date1 not equals date2', function() {
+            assert.equal(false, dateUtils.dateBeforeOrEquals(new Date(), new Date(0)));
+        });
+
+    });
+
 });
